bd2d22fc0e32d7caaeb7f4db90a89c6054d019e8
anushika1998 <anushika16015@iiitd.ac.in>
2020-08-27 16:29:12 +0530

    yello

--- checker.cpp
+++ checker.cpp
@@ -1,17 +1,21 @@
 #include <assert.h>
 
+const float[] bpmLimits={70,150};
+const float[] respLimits={30,95};
+const float[] spo2Limits={90, INT_MAX};
+
+bool isVitalOk(float val, float [] limits){
+  return val > limits[0] && val < limits[1];
+}
+
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 90) {
-    return false;
-  } else if(respRate < 30 || respRate > 95) {
-    return false;
-  }
-  return true;
+  return isVitalOk(bpm,bpmLimits) || isVitalOk(spo2,spo2Limits) || isVitalOk(respRate,respLimits);
 }
 
 int main() {
+  assert(isVitalOk(11,bpmLimits) == false);
+  assert(isVitalOk(440, spo2Limits) == true);
+  assert(isVitalOk(2324, respLimits) == false);
   assert(vitalsAreOk(80, 95, 60) == true);
   assert(vitalsAreOk(60, 90, 40) == false);
 }
